8b70affa9518e843e467e9fe63f74ccd7c96e1f2
aluno <aluno@lab01-09>
2026-03-10 14:13:48 -0300

    prontinho

--- Programacao01/src/main/java/com/mycompany/programacao01/Programacao01.java
+++ Programacao01/src/main/java/com/mycompany/programacao01/Programacao01.java
@@ -11,7 +11,27 @@ package com.mycompany.programacao01;
 public class Programacao01 {
     public static void main(String[] args) {
         Estudante leonardo = new Estudante();
+        Estudante camada = new Estudante();
+        Estudante jeffersondorme = new Estudante();
         
-        leonardo.getNome();
+        camada.setNome("Marco");
+        
+        System.out.println(camada.getNome());
+        System.out.println(leonardo.getNome());
+        System.out.println(jeffersondorme.getNome());
+        System.out.println("");
+        
+        leonardo.setNome("Leonardo");
+        System.out.println(camada.getNome());
+        System.out.println(leonardo.getNome());
+        System.out.println(jeffersondorme.getNome());
+        System.out.println("");
+        
+        jeffersondorme.setNome("Jefferon");
+        
+        System.out.println(camada.getNome());
+        System.out.println(leonardo.getNome());
+        System.out.println(jeffersondorme.getNome());
+        
     }
 }
