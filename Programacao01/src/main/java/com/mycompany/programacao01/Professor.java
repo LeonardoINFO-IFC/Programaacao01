918f2d676e85a6cb9e1178e16a7af3ac03597035
aluno <aluno@lab01-09>
2026-03-17 14:13:19 -0300

    pronto

---
+++ Programacao01/src/main/java/com/mycompany/programacao01/Professor.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.programacao01;
+
+import javax.xml.crypto.Data;
+
+/**
+ *
+ * @author aluno
+ */
+public class Professor {
+    
+    private static String nomeCpmpleto;
+    private String Siape;
+    private Data dataNacimento;
+    private String email;
+    private int telefoneContato;
+    private String tituloAcademica;
+    private String areaAtuação;
+    private int anoIngrssado;
+    private String formacao;
+    private String regimeTrabalho;
+}
