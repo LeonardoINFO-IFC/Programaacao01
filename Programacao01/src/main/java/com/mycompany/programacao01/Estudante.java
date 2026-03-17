781afca8dc172234ae2ed37bc5b19f388ba8cc41
aluno <aluno@lab01-09>
2026-03-17 14:13:07 -0300

    pronto

--- Programacao01/src/main/java/com/mycompany/programacao01/Estudante.java
+++ Programacao01/src/main/java/com/mycompany/programacao01/Estudante.java
@@ -12,69 +12,115 @@ import javax.xml.crypto.Data;
  * @author aluno
  */
 public class Estudante {
+
+    private String cpf;
     private static String nome;
     private Data dataNacimento;
-    private String cpf;
     private char genero;
-    private String matricula;
     private String email;
-    
-    public Estudante(){
+    private int telefoneContato;
+    private int anoIngrssado;
+    private int semestreIngresado;
+    private String situacaoAcademica;
+    private String matricula;
+    private String nivelEnsino;
+    private String numero;
+
+    public Estudante() {
         nome = "Nobody";
     }
-    
-    public int obterIdade(Data hoje){
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String _cpf) {
+        cpf = _cpf;
+    }
+
+    public int obterIdade(Data hoje) {
         int idade = 0;
         //Logica de calculo de idade
         return idade;
     }
-    
-    public static String getNome(){
+
+    public static String getNome() {
         return nome;
     }
-    
-    public static void setNome(String _nome){
+
+    public static void setNome(String _nome) {
         nome = _nome;
     }
-    
-    public Data getDataNacimento(){
+
+    public Data getDataNacimento() {
         return dataNacimento;
     }
-    
-    public void setDataNacimento(Data _dataNascimento){
+
+    public void setDataNacimento(Data _dataNascimento) {
         dataNacimento = _dataNascimento;
     }
-    
-    public String getCpf(){
-        return cpf;
-    }
-    
-    public void setCpf(String _cpf){
-        cpf = _cpf;
-    }
-    
-    public char getGenero(){
+
+    public char getGenero() {
         return genero;
     }
-    
-    public void setGenero(char _genero){
+
+    public void setGenero(char _genero) {
         genero = _genero;
     }
-    
-    public String getMatricula(){
-        return matricula;
-    }
-    
-    public void setMatricula(String _matricula){
-        matricula = _matricula;
-    }
-    
-    public String getEmail(){
+
+    public String getEmail() {
         return email;
     }
-    
-    public void setEmail(String _email){
+
+    public void setEmail(String _email) {
         email = _email;
     }
-    
+
+    public int getTelefoneContato() {
+        return telefoneContato;
+    }
+
+    public void setTelefoneContato(int _telefone) {
+        telefoneContato = _telefone;
+    }
+
+    public int getAnoIngressado() {
+        return anoIngrssado;
+    }
+
+    public void setAnoIngressado(int _anoIngreado) {
+        anoIngrssado = _anoIngreado;
+    }
+
+    public int getSemstreIngrssado() {
+        return semestreIngresado;
+    }
+
+    public void setSemestreIngressado(int _semestre) {
+        semestreIngresado = _semestre;
+    }
+
+    public String getSituacaoAcademica() {
+        return situacaoAcademica;
+    }
+
+    public void setSituacaoacademica(String _situacao) {
+        situacaoAcademica = _situacao;
+    }
+
+    public String getMatricula() {
+        return String.valueOf(anoIngrssado) + String.valueOf(semestreIngresado) + numero;
+    }
+
+    public void setMatricula(String _numero) {
+        numero = _numero;
+    }
+
+    public String getNivelEnsino() {
+        return nivelEnsino;
+    }
+
+    public void setNivelEnsino(String _nivelEnsino) {
+        nivelEnsino = _nivelEnsino;
+    }
 }
