c77daf8e85171aa4044a83400f2c4cc1ea01509f
aluno <aluno@lab01-08>
2026-03-23 09:22:39 -0300

    prontinho

---
+++ Programacao01/src/main/java/com/mycompany/programacao01/Disciplina.java
@@ -0,0 +1,81 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.programacao01;
+
+/**
+ *
+ * @author aluno
+ */
+public class Disciplina {
+
+    private String codigoDisci;
+    private String nome;
+    private int cargaHora;
+    private String ementa;
+    private String objetivo;
+    private String referencia;
+    private String infoAdici;
+    
+    public Disciplina(){
+        nome = "Noobod";
+    }
+    
+    public String getCodeDisci() {
+        return codigoDisci;
+    }
+
+    public void setCodeDisci(String _code) {
+        codigoDisci = _code;
+    }
+    
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String _nome) {
+        nome = _nome;
+    }
+    
+    public int getCarga() {
+        return cargaHora;
+    }
+
+    public void setCarga(int _carga) {
+        cargaHora = _carga;
+    }
+    
+    public String geEmenta() {
+        return ementa;
+    }
+
+    public void setEmenta(String _ementa) {
+        ementa = _ementa;
+    }
+    
+    public String getObjetivo() {
+        return objetivo;
+    }
+
+    public void setObjetivo(String _objetv) {
+        objetivo = _objetv;
+    }
+    
+    public String getReferencia() {
+        return referencia;
+    }
+
+    public void setReferencia(String _ref) {
+        referencia = _ref;
+    }
+    
+    public String getInfoAdici() {
+        return infoAdici;
+    }
+
+    public void setInfoAdici(String _InfoAdici) {
+        infoAdici = _InfoAdici;
+    }
+    
+}
--- Programacao01/src/main/java/com/mycompany/programacao01/Professor.java
+++ Programacao01/src/main/java/com/mycompany/programacao01/Professor.java
@@ -11,15 +11,99 @@ import javax.xml.crypto.Data;
  * @author aluno
  */
 public class Professor {
-    
-    private static String nomeCpmpleto;
-    private String Siape;
+
+    private static String nome;
+    private String siape;
     private Data dataNacimento;
     private String email;
     private int telefoneContato;
     private String tituloAcademica;
     private String areaAtuação;
-    private int anoIngrssado;
-    private String formacao;
     private String regimeTrabalho;
+    private int dataIngrssado;
+    private String formacao;
+
+    public Professor() {
+        nome = "Nobody";
+    }
+    
+    public static String getNome() {
+        return nome;
+    }
+
+    public static void setNome(String _nome) {
+        nome = _nome;
+    }
+    
+    public String getSiape() {
+        return siape;
+    }
+
+    public void setSiape(String _siape) {
+        siape = _siape;
+    }
+    
+    public Data getDataNasci() {
+        return dataNacimento;
+    }
+
+    public void setDataNasci(Data _dataNasci) {
+        dataNacimento = _dataNasci;
+    }
+    
+    public String getEmial() {
+        return email;
+    }
+
+    public void setEmail(String _email) {
+        email = _email;
+    }
+    
+    public int getTelefone() {
+        return telefoneContato;
+    }
+
+    public void setTelefone(int _teleone) {
+        telefoneContato = _teleone;
+    }
+    
+    public String getTituloAcad() {
+        return tituloAcademica;
+    }
+
+    public void setTituloAcad(String _tituacad) {
+        tituloAcademica = _tituacad;
+    }
+    
+    public String getAreAtu() {
+        return areaAtuação;
+    }
+
+    public void setAreAreAtu(String _areAtu) {
+        areaAtuação = _areAtu;
+    }
+    public String getRegTrab() {
+        return regimeTrabalho;
+    }
+
+    public void setRegTrab(String _regTrab) {
+        regimeTrabalho = _regTrab;
+    }
+    
+    public int getDataIngr() {
+        return dataIngrssado;
+    }
+
+    public void setDataIngr(int _data) {
+        dataIngrssado = _data;
+    }
+    
+    public String getForma() {
+        return formacao;
+    }
+
+    public void setForm(String _formacao) {
+        formacao = _formacao;
+    }
+    
 }
